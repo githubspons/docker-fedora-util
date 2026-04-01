762b2afeecda753df83cf3178c4a01d1112e2919
githubspons <docker@fedora-1.json>
2026-04-01 18:22:52 +0000

    Actualitzacio

--- setup-fedora.sh
+++ setup-fedora.sh
@@ -1,4 +1,22 @@
+echo "Introdueix el nom d'usuari:"
+read usuari
+echo "Contrasenya:"
+read pass
+echo "Introdueix el hostname:"
+read hostname
+
 # update i setup packets inicials
 dnf update -y
-dnf install nano mc tmux iputils
+dnf install nano mc tmux iputils sshd top -y
 
+#ssh
+/usr/libexec/openssh/sshd-keygen rsa
+/usr/libexec/openssh/sshd-keygen ecdsa
+/usr/libexec/openssh/sshd-keygen ed25519
+/usr/sbin/sshd
+
+# creació usuari
+
+adduser $usuari
+echo "$usuari:$pass" | sudo chpasswd
+passwd $pass
